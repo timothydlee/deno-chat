fc95c98b963b06db67ad7feac876ff60080cc941
Tim Lee <tim.lee@loanboss.com>
2021-07-21 00:41:57 -0400

    Changed to chat server

--- deno-chat-api/index.js
+++ deno-chat-api/index.js
@@ -1,9 +1,22 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const messages = [];
+
+const router = new Router();
+router
+    .get("/", (context) => {
+        context.response.body = "Chat server!";
+    })
+    .get("/messages", (context) => {
+        context.response.body = messages;
+    })
+    .get("/messages", async (context) => {
+        const message = await context.request.body().value;
+        messages.push(message);
+    });
 
 const app = new Application();
-
-app.use((ctx) => {
-    ctx.response.body = "Hello, third try";
-});
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 addEventListener('fetch', app.fetchEventHandler());
